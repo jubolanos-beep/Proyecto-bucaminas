174bde9ee5941f4d931872fa0713df5c55ed2b20
fgomezjo <fgomezjo@unal.edu.co>
2026-06-22 17:37:28 -0500

    estos son mis avances parciales, hace falta la lectura y escritura de archivos, poner el tiempo y arreglar cosas finales de jugabilidad

--- avancesFabianProyecto.cpp
+++ avancesFabianProyecto.cpp
@@ -6,18 +6,19 @@
 #include<sstream>
 #include<fstream>
 using namespace std;
-void mostrarInterfaz(){
+void mostrarInterfaz(){ //Esta funcion es para mostrar la interfaz al principio de todo, de esta manera hay interactividad con el usuario para que luego comienze a escojer lo que desea hacer
 	cout<<"-----MENU-----"<<endl;
 	cout<<"1. Jugar"<<endl;
 	cout<<"2. Ver puntajes"<<endl;
 	cout<<"3. Salir"<<endl;
 }
-void elegirDificultad(int &filas, int &columnas, int &minas){
+void elegirDificultad(int &filas, int &columnas, int &minas){ //Esta funcion tambien es interactiva ya que permite al usuario escojer una dificultad deseada
 	int opcion;
 	cout<<"Seleccione una dificultad: "<<endl;
 	cout<<"1. Facil(8x8, 10 minas)"<<endl;
 	cout<<"2. Medio(16x16, 40 minas)"<<endl;
 	cout<<"3. Dificil(16x30, 99 minas)"<<endl;
+	cout<<"Al escojer otro numero se seleccionara dificultad Facil por defecto"<<endl;
 	cout<<"Opcion: ";
 	cin>>opcion;
 	switch(opcion){
@@ -38,13 +39,13 @@ void elegirDificultad(int &filas, int &columnas, int &minas){
 			minas = 99;
 			break;
 		default:
-			cout<<"Opción no válida, se selecionará dificultad Fácil por defecto"<<endl;
+			cout<<"Opcion no valida, se selecionara dificultad Facil por defecto"<<endl;
 			filas = 8;
 			columnas = 8;
 			minas = 10;
 	}
 }
-void mostrarTablero(const vector<vector<char>>& tablero){
+void mostrarTablero(const vector<vector<char>>& tablero){ //Esta funcion muestra el tablero para que el jugador pueda ver los cambios durante el transcurso del juego
 	cout<<"  ";
 	for(int i = 0; i < tablero[0].size(); i++){
 		cout<<i<<" ";
@@ -59,7 +60,7 @@ void mostrarTablero(const vector<vector<char>>& tablero){
 		cout<<endl;
 	}
 }
-void colocarMinas(vector<vector<char>>& tablero, int minas){
+void colocarMinas(vector<vector<char>>& tablero, int minas){ //Esta funcion ayuda a colocar las minas en el tablero de manera aleatoria
 	int filas = tablero.size();
 	int columnas = tablero[0].size();
 	while(minas > 0){
@@ -74,28 +75,28 @@ void colocarMinas(vector<vector<char>>& tablero, int minas){
 int main(){
 	int opcion;
 	do{
-		mostrarInterfaz();
+		mostrarInterfaz(); //mostramos la interfaz
 		cout<<"Opcion: ";
 		cin>>opcion;
-		switch(opcion){
+		switch(opcion){ //aqui es donde el usuario elije que hacer a partir de la interfaz
 			case 1: {
 				int filas, columnas, minas;
 				elegirDificultad(filas, columnas, minas);
-				vector<vector<char>> tableroVisible(filas, vector<char>(columnas, '#'));
+				vector<vector<char>> tableroVisible(filas, vector<char>(columnas, '#')); //creamos dos tableros, uno visible y uno oculto para el usuario, donde se iran generando cambios y nos permite ver a nosotros como transcurre el juego durante la programacion
 				vector<vector<char>> tableroOculto(filas, vector<char>(columnas, '0'));
-				cout<<"Tablero creado: "<<filas<<" x "<<columnas<<endl;
-				colocarMinas(tableroOculto, minas);
-				mostrarTablero(tableroVisible);
-				mostrarTablero(tableroOculto);
+				cout<<"Tablero creado: "<<filas<<" x "<<columnas<<endl; //el usuario puede corroborar la dificultad escojida
+				colocarMinas(tableroOculto, minas); //colocamos las minas aleatoriamente
+				mostrarTablero(tableroVisible); //ver  parcialmente el tablero
+				mostrarTablero(tableroOculto); // esto se quitara luego pero ahora nos sirve para comparar con respecto al otro tablero durante la programacion del juego
 				break;
 			}
 			case 2:{
-				cout<<"Mostrando puntajes: "<<endl;
+				cout<<"Mostrando puntajes: "<<endl; //aca se mostraran puntajes, falta por terminar
 				break;
 			}
 		}
 		
-	}while(opcion != 3);
+	}while(opcion != 3); //permite que si se escoje 3, termina el programa, ademas, si se scoje un numero diferente volvera a aparecer la interfaz hasta que el usuario decida jugar o ver los puntajes.
 	
 	return 0;
 }
